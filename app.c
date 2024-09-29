783b26c2297dddd678cee4cfb43308c438a36ccb
timchenkovich <367562@niuitmo.ru>
2024-09-29 17:40:31 +0300

    Added radius, step

--- app.c
+++ app.c
@@ -1,5 +1,11 @@
 #include "sim.h"
 
+// -------META PARAMS-------
+const int RADIUS = 30;
+const int STEP = 3;
+
+// -------GLOBALS-------
+
 // Current box position
 int CUR_X;
 int CUR_Y;
@@ -12,10 +18,12 @@ int COLOR;
 int BACKGROUND_COLOR;
 int BC_DELTA;
 
+// -------CODE-------
+
 void init()
 {
-    CUR_X = simRand() % (SIM_X_SIZE - 11);
-    CUR_Y = simRand() % (SIM_Y_SIZE - 11);
+    CUR_X = simRand() % (SIM_X_SIZE - 2 * RADIUS - STEP);
+    CUR_Y = simRand() % (SIM_Y_SIZE - 2 * RADIUS - STEP);
     DIR_X = (simRand() % 2) == 0 ? -1 : 1;
     DIR_Y = (simRand() % 2) == 0 ? -1 : 1;
 
@@ -37,12 +45,12 @@ void fill_background()
 
 void adjust_directions()
 {
-    if (CUR_X + 11 >= SIM_X_SIZE || CUR_X <= 0)
+    if (CUR_X + 2 * RADIUS + STEP >= SIM_X_SIZE || CUR_X - (STEP - 1) <= 0)
     {
         DIR_X = -DIR_X;
     }
 
-    if (CUR_Y <= 0 || CUR_Y + 11 >= SIM_Y_SIZE)
+    if (CUR_Y - (STEP - 1) <= 0 || CUR_Y + 2 * RADIUS + STEP >= SIM_Y_SIZE)
     {
         DIR_Y = -DIR_Y;
     }
@@ -51,8 +59,8 @@ void adjust_directions()
 void adjust_positions()
 {
     adjust_directions();
-    CUR_X += DIR_X;
-    CUR_Y += DIR_Y;
+    CUR_X += DIR_X * STEP;
+    CUR_Y += DIR_Y * STEP;
 }
 
 void change_colors()
@@ -66,51 +74,45 @@ void change_colors()
     BACKGROUND_COLOR += BC_DELTA;
 }
 
-void draw_happy()
+void reflect_dots(int xc, int yc, int x, int y)
 {
-    for (int i = 3; i >= 0; --i)
+    simPutPixel(xc + x, yc + y, COLOR);
+    simPutPixel(xc - x, yc + y, COLOR);
+    simPutPixel(xc + y, yc + x, COLOR);
+    simPutPixel(xc - y, yc + x, COLOR);
+}
+
+// dir == 1, if down half, dir == -1, otherwise
+void draw_half_circle(int xc, int yc, int r, int dir)
+{
+    int x = 0, y = r;
+    int d = 3 - 2 * r;
+
+    reflect_dots(xc, yc, dir * x, dir * y);
+
+    while (y >= x)
     {
-        for (int j = i; j < i + 2; ++j)
+
+        if (d > 0)
         {
-            simPutPixel(CUR_X + j, CUR_Y + (3 - i), COLOR);
+            y--;
+            d = d + 4 * (x - y) + 10;
+        }
+        else
+        {
+            d = d + 4 * x + 6;
         }
 
-        for (int j = 10 - i; j > 8 - i; --j)
-        {
-            simPutPixel(CUR_X + j, CUR_Y + (3 - i), COLOR);
-        }
+        x++;
+
+        reflect_dots(xc, yc, dir * x, dir * y);
     }
+}
 
-    for (int i = 4; i < 7; ++i)
-    {
-        simPutPixel(CUR_X, CUR_Y + i, COLOR);
-        simPutPixel(CUR_X + 10, CUR_Y + i, COLOR);
-    }
-
-    for (int i = 3; i >= 0; --i)
-    {
-        for (int j = i; j < i + 2; ++j)
-        {
-            simPutPixel(CUR_X + j, CUR_Y + 10 - (3 - i), COLOR);
-        }
-
-        for (int j = 10 - i; j > 8 - i; --j)
-        {
-            simPutPixel(CUR_X + j, CUR_Y + 10 - (3 - i), COLOR);
-        }
-    }
-
-    simPutPixel(CUR_X + 5, CUR_Y, COLOR);
-    simPutPixel(CUR_X + 5, CUR_Y + 10, COLOR);
-
-    simPutPixel(CUR_X + 4, CUR_Y + 3, COLOR);
-    simPutPixel(CUR_X + 6, CUR_Y + 3, COLOR);
-
-    simPutPixel(CUR_X + 3, CUR_Y + 6, COLOR);
-    simPutPixel(CUR_X + 4, CUR_Y + 7, COLOR);
-    simPutPixel(CUR_X + 5, CUR_Y + 7, COLOR);
-    simPutPixel(CUR_X + 6, CUR_Y + 7, COLOR);
-    simPutPixel(CUR_X + 7, CUR_Y + 6, COLOR);
+void draw_circle(int xc, int yc, int r)
+{
+    draw_half_circle(xc, yc, r, 1);
+    draw_half_circle(xc, yc, r, -1);
 }
 
 void app()
@@ -120,7 +122,12 @@ void app()
     while (1)
     {
         fill_background();
-        draw_happy();
+
+        draw_circle(CUR_X + RADIUS, CUR_Y + RADIUS, RADIUS);
+        draw_circle(CUR_X + 2 * RADIUS / 3, CUR_Y + 2 * RADIUS / 3, RADIUS / 5);
+        draw_circle(CUR_X + 4 * RADIUS / 3, CUR_Y + 2 * RADIUS / 3, RADIUS / 5);
+        draw_half_circle(CUR_X + RADIUS, CUR_Y + 11 * RADIUS / 10, RADIUS / 2, 1);
+
         adjust_positions();
         change_colors();
 
