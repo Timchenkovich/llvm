2e5518184817ca44bbde49c91b5a832071e301d2
Timchenkovich <lilsk8rrr@gmail.com>
2025-03-08 20:26:48 +0000

    Better naming

--- asm/cmake/BootstrapIsa.cmake
+++ asm/cmake/BootstrapIsa.cmake
@@ -19,9 +19,9 @@ macro(bootstrap_isa isaPathStr)
     endforeach()
 
 
-    set(${isaName}_INSTRUCTION_LEXER "${CMAKE_CURRENT_BINARY_DIR}/${outputDir}/${isaName}InstructionLexer.g4")
-    set(${isaName}_INSTRUCTION_PARSER "${CMAKE_CURRENT_BINARY_DIR}/${outputDir}/${isaName}InstructionParser.g4")
-    set(${isaName}_GRAMMAR_SOURCES ${${isaName}_INSTRUCTION_PARSER} ${${isaName}_INSTRUCTION_LEXER})
+    set(${isaName}_INSTRUCTIONS_LEXER "${CMAKE_CURRENT_BINARY_DIR}/${outputDir}/${isaName}InstructionLexer.g4")
+    set(${isaName}_INSTRUCTIONS_PARSER "${CMAKE_CURRENT_BINARY_DIR}/${outputDir}/${isaName}InstructionParser.g4")
+    set(${isaName}_INSTRUCTIONS_GRAMMAR_SOURCES ${${isaName}_INSTRUCTIONS_PARSER} ${${isaName}_INSTRUCTIONS_LEXER})
 
 
     add_custom_command(
@@ -29,7 +29,7 @@ macro(bootstrap_isa isaPathStr)
     COMMAND mkdir -p ${outputDir} && cd ${outputDir} && ${CMAKE_BINARY_DIR}/asm/isa-creator/bootstrapIsa ${isaPath}
     DEPENDS ${CMAKE_BINARY_DIR}/asm/isa-creator/bootstrapIsa ${isaPath})
 
-    add_custom_target(generateInstructionGrammar DEPENDS ${${isaName}_GRAMMAR_SOURCES})
+    add_custom_target(generateInstructionGrammar DEPENDS ${${isaName}_INSTRUCTIONS_GRAMMAR_SOURCES})
 
     set(${isaName}_GENERATED_DIR ${CMAKE_CURRENT_BINARY_DIR}/${outputDir})
     set(${isaName}_ISA_SOURCES ${${isaName}_GENERATED_SOURCES})
